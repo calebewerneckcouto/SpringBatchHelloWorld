868f448cdb33a00cee0ad5dd4d719db00302774c
calebewerneckcouto <calebewerneck@hotmail.com>
2025-01-06 16:09:02 -0300

    continuando a refatoração do projeto

--- src/main/java/com/devsuperior/springbatch/tasklet/PrintHelloTasklet.java
+++ src/main/java/com/devsuperior/springbatch/tasklet/PrintHelloTasklet.java
@@ -5,14 +5,17 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 @Component
 @StepScope
 public class PrintHelloTasklet implements Tasklet{
+	@Value("${name}")
+	private String name;
 
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-		System.out.println("Olá Mundo"); // Exibe uma mensagem no console
+		System.out.println("Olá " + name + " !"); // Exibe uma mensagem no console
         return RepeatStatus.FINISHED; // Indica que o Step foi concluído com sucesso
 	}
 
